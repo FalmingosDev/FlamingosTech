722b2412c9f022495bbf61d0828532c20499fd5c
RohitChanda <89380775+FalmingosDev@users.noreply.github.com>
2021-11-20 18:14:56 +0530

    20-11-21

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+// import { Component } from '@angular/core';
 
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
+// })
+// export class AppComponent {
+//   title = 'flamingostech';
+// }
+
+//////////////////////////////////////////////////////
+
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+  
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'flamingostech';
+export class AppComponent implements OnInit {
+    constructor(private router: Router) { }
+    title = 'flamingostech';
+      
+    ngOnInit() {
+        this.router.events.subscribe((event) => {
+            if (!(event instanceof NavigationEnd)) {
+                return;
+            }
+            window.scrollTo(0, 0)
+        });
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,66 @@
+// import { HttpClientModule } from '@angular/common/http';
+// import { NgModule } from '@angular/core';
+// import { BrowserModule } from '@angular/platform-browser';
+// import { SlickCarouselModule } from 'ngx-slick-carousel';
+// import { AppRoutingModule } from './app-routing.module';
+// import { AppComponent } from './app.component';
+// import { HomeModule } from './home/home.module';
+// import { PageNotFoundModule } from './page-not-found/page-not-found.module';
+// import { ProductsModule } from './products/products.module';
+// import { SolutionModule } from './solution/solution.module';
+
+ 
+// @NgModule({
+//   declarations: [
+//     AppComponent
+//   ],
+//   imports: [
+//     BrowserModule,
+//     AppRoutingModule,
+//     HomeModule,
+//     ProductsModule,
+//     SolutionModule,
+//     PageNotFoundModule,
+//     SlickCarouselModule,
+//     HttpClientModule
+//   ],
+//   providers: [],
+//   bootstrap: [AppComponent]
+// })
+// export class AppModule { }
+/////////////////////////////////////////////////////////////////
+
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 import { SlickCarouselModule } from 'ngx-slick-carousel';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeModule } from './home/home.module';
-import { PageNotFoundModule } from './page-not-found/page-not-found.module';
-import { ProductsModule } from './products/products.module';
 import { SolutionModule } from './solution/solution.module';
-
-
+// import { PageNotFoundModule } from './page-not-found/page-not-found.module';
+import { ProductsModule } from './products/products.module';
+   
+const routes: Routes = [
+  { path: '', component: AppComponent },//
+];
+   
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HomeModule,
-    ProductsModule,
-    SolutionModule,
-    PageNotFoundModule,
-    SlickCarouselModule,
-    HttpClientModule
-  ],
+  imports:
+      [   BrowserModule, 
+        RouterModule.forRoot(routes),//
+        AppRoutingModule,
+        HomeModule,
+        SlickCarouselModule,
+        SolutionModule,
+        HttpClientModule,
+        ProductsModule
+       ],
+  declarations: [ AppComponent ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
+
