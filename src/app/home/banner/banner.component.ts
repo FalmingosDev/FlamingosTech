83028dbee076d4e277035c4ad9c40cc4822f2307
Durba <89380775+FalmingosDev@users.noreply.github.com>
2021-12-31 12:05:38 +0530

    alt tag

--- src/app/home/banner/banner.component.ts
+++ src/app/home/banner/banner.component.ts
@@ -15,12 +15,12 @@ export class BannerComponent implements OnInit {
     "autoplaySpeed": 1500
   };
   slideImg = [
-    { img: "assets/images/banner_bg1.png" },
-    { img: "assets/images/banner_bg2.png" },
-    { img: "assets/images/banner_bg3.png" },
-    { img: "assets/images/banner_bg4.png" },
-    { img: "assets/images/banner_bg5.png" },
-    { img: "assets/images/banner_bg6.png" }
+    { img: "assets/images/banner_bg1.png", alt:"It company in kolkata" },
+    { img: "assets/images/banner_bg2.png", alt:"Online It service in kolkata" },
+    { img: "assets/images/banner_bg3.png", alt:"Company in kolkata with It service" },
+    { img: "assets/images/banner_bg4.png", alt:"Best software company in kolkata" },
+    { img: "assets/images/banner_bg5.png", alt:"Digital marketing agency kolkata" },
+    { img: "assets/images/banner_bg6.png", alt:"Best kolkata it company" }
   ];
   slidetext = [
     { text: `Cloud based custom <span>ERP & CRM</span> Application` },
